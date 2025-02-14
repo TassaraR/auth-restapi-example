8e52640e54e542ccb39229259bb0856460efd299
Renzo Tassara <renzotassara@192.168.1.31>
2025-02-14 17:43:05 -0300

    Change instance methods for class methods

--- api/password.py
+++ api/password.py
@@ -9,9 +9,11 @@ logging.getLogger("passlib").setLevel(logging.ERROR)
 class PasswordManager:
     _context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-    def verify_password(self, plain_password: str, hashed_password: str) -> bool:
+    @classmethod
+    def verify_password(cls, plain_password: str, hashed_password: str) -> bool:
         """Verifies is user password is correct"""
-        return self._context.verify(secret=plain_password, hash=hashed_password)
+        return cls._context.verify(secret=plain_password, hash=hashed_password)
 
-    def get_password_hash(self, password: str) -> str:
-        return self._context.hash(password)
+    @classmethod
+    def get_password_hash(cls, password: str) -> str:
+        return cls._context.hash(password)
